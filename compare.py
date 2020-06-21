1745b872f0f8fc6445b78c6e83f30eca0d937a2f
Lut99 <mullertim99@gmail.com>
2020-06-21 14:10:15 +0200

    Did some benchmarks and stuff

---
+++ compare.py
@@ -0,0 +1,15 @@
+# COMPARE.py
+#   by Anonymous
+#
+# Created:
+#   21/06/2020, 14:08:50
+# Last edited:
+#   21/06/2020, 14:08:50
+# Auto updated?
+#   Yes
+#
+# Description:
+#   Simple script that compares the runtimes line-by-line
+#
+
+import pandas as pd
